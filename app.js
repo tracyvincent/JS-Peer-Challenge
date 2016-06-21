e54f77ccbb6f460734f32bd59e1336c5d352a1e6
tracyvincent <tracykvincent@gmail.com>
2016-06-21 14:13:52 -0500

    Initial Commit

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
 var provided = 'Hello World!';
+
+//step 1
+console.log(provided.charAt(1).length);
+
+//step 2
+console.log(Number(provided));
+
+//step 3
+console.log(provided.substring(0,5).split(''));
+
+//step 4
+console.log(typeof provided);
